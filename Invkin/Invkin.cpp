af81b47a7558cec46815948349a70d0e821613aa
SamEmb <40595362+SamEmb@users.noreply.github.com>
2018-12-20 00:19:19 +0530

    Update Invkin.cpp

--- Invkin/Invkin.cpp
+++ Invkin/Invkin.cpp
@@ -1,49 +1,37 @@
 #include "Arduino.h"
 #include "Invkin.h"
-Invkin::Invkin(float X,float Y,float Z){
-    x =  X ;
-    y =  Y ;
-    z =  Z ;
-   
-   const float cx=2; //coxa
-   const float fm=6.2; //femur
-   const float tb=8.3; // tibia 
-   
-   float L, L1;
-   float alpha, alpha1,alpha2,beta,gama;
+Invkin::Invkin(float x,float y,float z){
+    X =  x ;
+    Y =  y ;
+    Z =  z ;   
 }
 
 void Invkin::transform()
 {
     
-trigono_xyz(x,y,z); //contoh x,y,z
-Serial.print("gama= ");
-Serial.print(gamma);
-Serial.print(", alpha= ");
-Serial.print(alpha);
-Serial.print(", beta= ");
-Serial.print(beta);    
+
+    
+trigono_xyz(X,Y,Z); //contoh x,y,z
+    
+Serial.print("theta1= ");
+Serial.print(theta1);
+Serial.print(", theta2= ");
+Serial.print(theta2);
+Serial.print(", theta3= ");
+Serial.print(theta3);    
 Serial.println();
     
 }
 
-void Invkin::trigono_xyz(float x, float y, float z)
+void Invkin::trigono_xyz(float X, float Y, float Z)
 {
     
- L1=sqrt(sq(x)+sq(y));
-    
- gamma=atan(x/y)/PI*180;
-    
- L=sqrt(sq(L1-cx)+sq(z));
-    
- beta=acos((sq(tb)+sq(fm)-sq(L))/(2*tb*fm))/PI*180;
-    
- alpha1=acos(z/L)/PI*180;
-    
- alpha2=acos((sq(fm)+sq(L)-sq(tb))/(2*fm*L))/PI*180;
-    
- alpha=alpha1+alpha2;
-    
- 
+ theta1 = (atan(Y/X))*180/PI;
+
+    r3 = sqrt(X*X + Y*Y + (Z-a1)*(Z-a1));
+
+    theta2 = (atan((Z-a1)/(sqrt(X*X+Y*Y)))-acos((a2*a2 + r3*r3 - a3*a3)/(2*a2*r3)))*180/PI;
+
+    im = acos((a3*a3 + a2*a2 - r3*r3)/(2*a2*a3))*(180/PI);    
     
 }
