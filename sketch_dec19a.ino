bff77ba018949f056d841c568bdc105c615af289
SamEmb <40595362+SamEmb@users.noreply.github.com>
2018-12-21 16:39:37 +0530

    Add files via upload

--- sketch_dec19a.ino
+++ sketch_dec19a.ino
@@ -1,6 +1,6 @@
 
-#include <ros.h>
-#include <geometry_msgs/Point.h>
+//#include <ros.h>
+//#include <geometry_msgs/Point.h>
 
 #include "Invkin.h"
 #include "MotorPID.h"
@@ -12,16 +12,16 @@ float x ;
 float y ;
 float z ;
 
-ros::NodeHandle node;
-geometry_msgs::Point msg;
+//ros::NodeHandle node;
+//geometry_msgs::Point msg;
 
-MotorPID M1(3,22,A0);
+MotorPID M1(3,22,A0);   // pwm , dir , potenio 
+MotorPID M2(4,23,A1);
+MotorPID M3(5,24,A2);
 
-Invkin IK(2,4,6);
 
 
-
-void roverCallBack(const geometry_msgs::Point& co_ordinates)
+/*void roverCallBack(const geometry_msgs::Point& co_ordinates)
 {  
     X =   co_ordinates.x ;
     Y =   co_ordinates.y ;  
@@ -29,38 +29,43 @@ void roverCallBack(const geometry_msgs::Point& co_ordinates)
     Serial.println(X);
    
 }
+*/
 
 
-
-ros::Subscriber <geometry_msgs::Point> sub("co_ordinates", &roverCallBack);
+//ros::Subscriber <geometry_msgs::Point> sub("co_ordinates", &roverCallBack);
 
 void setup()
 {
   Serial.begin(115200);  
-  node.getHardware()->setBaud(115200);
+  
+/*  node.getHardware()->setBaud(115200);
   node.initNode();
   node.subscribe(sub);
+*/  
+  
   pinMode(A0,INPUT);
   pinMode(3,OUTPUT);
   pinMode(22,OUTPUT);
-  M1.setPID(0.5,0.02,0.02);
   
+  M1.setPID(0.9,0.02,0.02);  //elbow
+  M2.setPID(0.85,0.02,0.02); //shoulder
+  M3.setPID(1.5.0.02.0.02);  //base
 
   
 } 
 
 void loop()
 {
-   IK.transform(); 
-  M1.x = IK.theta1 ;
-  int m = analogRead(A0);
-  Serial.println(M1.x);
-  Serial.println(map(m,0,1023,0,3600));
+  IK.transform();
   
-  
+  M1.x = IK.theta1;  // angle for motor 1 
+  M2.x = IK.theta2;  // angle for motor 2
+  M3.x = IK.theta3;  // angle for motor 3
   
   M1.errorcheck();
+  M2.errorcheck();
+  M3.errorcheck();
     
-  node.spinOnce(); 
-  delay(1000);
+ // node.spinOnce(); 
+  
 }
