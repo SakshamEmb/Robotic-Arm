ef38fd20800bd3285b50f62d4147ee2ab3442c82
SamEmb <40595362+SamEmb@users.noreply.github.com>
2018-12-23 19:39:54 +0530

    Add files via upload

---
+++ POT.ino
@@ -0,0 +1,32 @@
+#include <MotorPID.h>
+
+
+MotorPID motor1(3,22,A0,50);
+
+//MotorPID motor2(5,24,A0,50);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  motor1.setPID(0.39,0.01,-0.002);
+ // motor2.setPID(0.25,0.039,0.02);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int y;
+  float angle ;  
+  //y = analogRead(A0);
+  //angle = map(y,0,1023,0,3600); 
+  //Serial.print("Value of pot:");
+  //Serial.println(angle);
+  motor1.x = 200 ;
+ // motor2.x = 600 ; 
+  Serial.print("Setpoint: ");
+  Serial.println(motor1.x);
+  motor1.errorcheck();
+  //motor2.errorcheck();
+  delay(1000);
+  
+}
